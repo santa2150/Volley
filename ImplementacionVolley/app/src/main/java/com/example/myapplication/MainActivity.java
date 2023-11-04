4a54bedd7848b7babd0049e89f998e14dd352a5a
jdnaranjo1411 <112138324+jdnaranjo1411@users.noreply.github.com>
2023-11-04 11:39:32 -0500

    Cargado en mani activity
    En teoria ya esta cargada la api en main activity

--- ImplementacionVolley/app/src/main/java/com/example/myapplication/MainActivity.java
+++ ImplementacionVolley/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,50 @@
-package com.example.myapplication;
+package com.example.gfgvolleyapicall;
+
+import static android.content.ContentValues.TAG;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+    private RequestQueue mRequestQueue;
+    private StringRequest mStringRequest;
+    private String url = "https://rickandmortyapi.com/api";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getData();
+    }
+
+    private void getData() {
+
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        mStringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Toast.makeText(getApplicationContext(), "Response :" + response.toString(), Toast.LENGTH_LONG).show();//display the response on screen
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i(TAG, "Error :" + error.toString());
+            }
+        });
+
+        mRequestQueue.add(mStringRequest);
     }
 }
